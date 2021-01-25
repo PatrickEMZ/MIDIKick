99aa64985a73179dc4f7dbf17f0307020381320c
PatrickEMZ <patrick.mazeau@libertysurf.fr>
2021-01-25 13:48:39 +0100

    Update UIMidiKick.h

--- MidiKick/UIMidiKick.h
+++ MidiKick/UIMidiKick.h
@@ -1,14 +1,21 @@
 // -*-c++-*-
+// UI for MidiKick
+// Licence:
+// - Attribution 4.0 International (CC BY 4.0):
+//   You are free to:
+//     Share ― copy and redistribute the material in any medium or format
+//     Adapt ― remix, transform, and build upon the material for any purpose, even commercially.
+//   Under the following terms:
+//     Attribution ― You must give appropriate credit, provide a link to the license, and indicate if changes were made.
+//            You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+//     No additional restrictions ― You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+//
 
-// Gestion de menu
-
-#ifndef UIPOULAILLER_H
-#define UIPOULAILLER_H
+#ifndef UIMIDIKICK_H
+#define UIMIDIKICK_H
 
 #include <Arduino.h>
-//#include <LiquidCrystal.h>
 #include <rgb_lcd.h>
-//#include <LiquidCrystal_I2C.h>
 #include <LinkedList.h>
 #include "ElementUI.h"
 
