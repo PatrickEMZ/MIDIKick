ccd87d8098e218fe80fa076348d436f83d990932
PatrickEMZ <patrick.mazeau@libertysurf.fr>
2021-01-25 14:01:46 +0100

    Update Memory.h

--- MidiKick/Memory.h
+++ MidiKick/Memory.h
@@ -1,4 +1,19 @@
 // -*-c++-*-
+// Memory: class to manage access to EEPROM
+//
+// Licence:
+// - Attribution 4.0 International (CC BY 4.0):
+//   You are free to:
+//     Share — copy and redistribute the material in any medium or format
+//     Adapt — remix, transform, and build upon the material for any purpose, even commercially.
+//   Under the following terms:
+//     Attribution — You must give appropriate credit, provide a link to the license, and indicate if changes were made.
+//            You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+//     No additional restrictions — You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+//
+
+#ifndef MEMORY_H
+#define MEMORY_H
 
 #include <EEPROM.h>
 
@@ -53,4 +68,4 @@ public:
   EEint curbVelocity;
 };
 
-  
+#endif  
